6d828d8b59d75308d205ac52f255b05cb66c2b89
Weiqiang <zhuwq0@gmail.com>
2023-06-19 00:09:21 -0700

    add double difference

--- adloc_dd_v1.py
+++ adloc_dd_v1.py
@@ -37,8 +37,8 @@ picks = pd.read_csv(data_path / "picks_gamma.csv", delimiter="\t", parse_dates=[
 events = pd.read_csv(data_path / "catalog_gamma.csv", delimiter="\t", parse_dates=["time"])
 
 # %%
-events = events[events["event_index"] < 100]
-picks = picks[picks["event_index"] < 100]
+events = events[events["event_index"] < 50]
+picks = picks[picks["event_index"] < 50]
 
 # %%
 proj = Proj(f"+proj=sterea +lon_0={config['center'][0]} +lat_0={config['center'][1]} +units=km")
@@ -77,25 +77,42 @@ plt.axis("scaled")
 plt.savefig(figure_path / "station_event_v1.png", dpi=300, bbox_inches="tight")
 
 # %%
-event_index = []
+event_index1 = []
+event_index2 = []
 station_index = []
 phase_score = []
 phase_time = []
 phase_type = []
 
-for i in range(len(events)):
-    phase_time.append(picks[picks["event_index"] == events.loc[i, "event_index"]]["phase_time"].values)
-    phase_score.append(picks[picks["event_index"] == events.loc[i, "event_index"]]["phase_score"].values)
-    phase_type.extend(picks[picks["event_index"] == events.loc[i, "event_index"]]["phase_type"].values.tolist())
-    event_index.extend([i] * len(picks[picks["event_index"] == events.loc[i, "event_index"]]))
-    station_index.append(
-        stations.loc[picks[picks["event_index"] == events.loc[i, "event_index"]]["station_id"], "index"].values
-    )
+event_index_map = {x: i for i, x in enumerate(events["event_index"].unique())}
+picks = picks[picks["event_index"]!=-1]
+picks["index"] = picks["event_index"].apply(lambda x: event_index_map[x])
+picks_by_event = picks.groupby("index")
+for key1, group1 in picks_by_event:
+    if key1 == -1:
+        continue
+    for key2, group2 in picks_by_event:
+        if key2 == -1:
+            continue
+        if key1 >= key2:
+            continue
+        # print(group1)
+        # print(group2)
+        common = group1.merge(group2, on=["station_id", "phase_type"], how="inner")
+        
+        phase_time.append(common["phase_time_x"].values - common["phase_time_y"].values)
+        phase_score.append(common["phase_score_x"].values * common["phase_score_y"].values)
+        phase_type.extend(common["phase_type"].values.tolist())
+        event_index1.extend([key1] * len(common))
+        event_index2.extend([key2] * len(common))
+        station_index.append(
+            stations.loc[common["station_id"], "index"].values
+        )
 
 phase_time = np.concatenate(phase_time)
 phase_score = np.concatenate(phase_score)
 phase_type = np.array([{"P": 0, "S": 1}[x.upper()] for x in phase_type])
-event_index = np.array(event_index)
+event_index = np.column_stack([event_index1, event_index2])
 station_index = np.concatenate(station_index)
 
 # %%
@@ -155,27 +172,34 @@ class TravelTime(nn.Module):
         pred_time = torch.zeros(len(phase_type), 1, dtype=torch.float32)
         for type in [0, 1]:
             station_index_ = station_index[phase_type == type]
-            event_index_ = event_index[phase_type == type]                
+            event_index_ = event_index[phase_type == type]
             phase_weight_ = phase_weight[phase_type == type]
 
             station_loc_ = self.station_loc(station_index_)
             station_dt_ = self.station_dt(station_index_)[:, type].unsqueeze(-1)
+            if double_difference:
+                station_loc_ = station_loc_.unsqueeze(1)
+                station_dt_ = station_dt_.unsqueeze(1)
 
             event_loc_ = self.event_loc(event_index_)
             event_time_ = self.event_time(event_index_)
 
             tt_ = self.calc_time(event_loc_, station_loc_, type)
             t_ = event_time_ + tt_ + station_dt_
-            pred_time[phase_type == type] = t_
-
-            if double_difference:
-                t_ = t_[0] - t_[1]
+            if not double_difference:
+                pred_time[phase_type == type] = t_
+            else:
+                pred_time[phase_type == type] = (t_[:, 0, :] - t_[:, 1, :])
 
             if phase_time is not None:
                 phase_time_ = phase_time[phase_type == type]
-                # loss = torch.mean(phase_weight * (t - phase_time) ** 2)
-                loss += torch.mean(F.huber_loss(tt_ + station_dt_, phase_time_ - event_time_, reduction="none") * phase_weight_)
-                loss += self.reg * torch.mean(torch.abs(station_dt_)) ## prevent the trade-off between station_dt and event_time
+                if not double_difference:
+                    # loss = torch.mean(phase_weight * (t - phase_time) ** 2)
+                    loss += torch.mean(F.huber_loss(tt_ + station_dt_, phase_time_ - event_time_, reduction="none") * phase_weight_)
+                    loss += self.reg * torch.mean(torch.abs(station_dt_)) ## prevent the trade-off between station_dt and event_time
+                else:
+                    dt = t_[:, 0, :] - t_[:, 1, :]
+                    loss += torch.mean(F.huber_loss(dt, phase_time_, reduction="none") * phase_weight_)
 
         return {"phase_time": pred_time, "loss": loss}
 
@@ -192,16 +216,15 @@ travel_time = TravelTime(
     event_time=event_time,
     velocity={"P": vp, "S": vs},
 )
-tt = travel_time(station_index, event_index, phase_type, phase_weight=phase_weight)["phase_time"]
+tt = travel_time(station_index, event_index, phase_type, phase_weight=phase_weight, double_difference=True)["phase_time"]
 print("Loss using true location: ", F.mse_loss(tt, phase_time))
 
 # %%
-# travel_time = TravelTime(num_event, num_station, station_loc, velocity={"P": vp, "S": vs})
-travel_time = TravelTime(num_event, num_station, station_loc, event_time=event_time, velocity={"P": vp, "S": vs})
-tt = travel_time(station_index, event_index, phase_type, phase_weight=phase_weight)["phase_time"]
-print("Loss using init location", F.mse_loss(tt, phase_time))
-init_event_loc = travel_time.event_loc.weight.clone().detach().numpy()
-init_event_time = travel_time.event_time.weight.clone().detach().numpy()
+# travel_time = TravelTime(num_event, num_station, station_loc, event_time=event_time, velocity={"P": vp, "S": vs})
+# tt = travel_time(station_index, event_index, phase_type, phase_weight=phase_weight, double_difference=True)["phase_time"]
+# print("Loss using init location", F.mse_loss(tt, phase_time))
+# init_event_loc = travel_time.event_loc.weight.clone().detach().numpy()
+# init_event_time = travel_time.event_time.weight.clone().detach().numpy()
 
 # # %%
 # optimizer = optim.LBFGS(params=travel_time.parameters(), max_iter=1000, line_search_fn="strong_wolfe")
@@ -218,14 +241,14 @@ init_event_time = travel_time.event_time.weight.clone().detach().numpy()
 optimizer = optim.Adam(params=travel_time.parameters(), lr=0.1)
 for i in range(1000):
     optimizer.zero_grad()
-    loss = travel_time(station_index, event_index, phase_type, phase_time, phase_weight)["loss"]
+    loss = travel_time(station_index, event_index, phase_type, phase_time, phase_weight, double_difference=True)["loss"]
     loss.backward()
     optimizer.step()
     if i % 100 == 0:
         print(f"Loss: {loss.item()}")
 
 # %%
-tt = travel_time(station_index, event_index, phase_type, phase_weight=phase_weight)["phase_time"]
+tt = travel_time(station_index, event_index, phase_type, phase_weight=phase_weight, double_difference=True)["phase_time"]
 print("Loss using invert location", F.mse_loss(tt, phase_time))
 station_dt = travel_time.station_dt.weight.clone().detach().numpy()
 print(f"station_dt: max = {np.max(station_dt)}, min = {np.min(station_dt)}, mean = {np.mean(station_dt)}")
@@ -243,10 +266,10 @@ plt.axis("scaled")
 plt.colorbar()
 xlim = plt.xlim()
 ylim = plt.ylim()
-plt.plot(init_event_loc[:, 0], init_event_loc[:, 1], "x", markersize=1, color="green", label="Initial locations")
+# plt.plot(init_event_loc[:, 0], init_event_loc[:, 1], "x", markersize=1, color="green", label="Initial locations")
 plt.plot(invert_event_loc[:, 0], invert_event_loc[:, 1], "x", markersize=1, color="red", label="Inverted locations")
 # plt.xlim(xlim)
 # plt.ylim(ylim)
 plt.legend()
-plt.savefig(figure_path / "invert_location_v1.png", dpi=300, bbox_inches="tight")
+plt.savefig(figure_path / "invert_location_dd_v1.png", dpi=300, bbox_inches="tight")
 # %%
